bdfe36950504e78c2d64bdab64a9cbc27fcd31b3
anshigandhi2002 <anshigandhi2002@gmail.com>
2025-04-08 03:22:50 +0530

    Added URL based KG generation

--- generate_KG.py
+++ generate_KG.py
@@ -5,6 +5,7 @@ import torch
 
 # knowldege base class
 from Knowledge_Base import KB
+from Knowledge_Base_url import KB_url
 
 # wrapper for wikipedia API
 import wikipedia
@@ -136,6 +137,104 @@ def from_text_to_kb(text, span_length=128, verbose=False):
 
     return kb
 
+# extract text from url, extract relations and populate the KB
+def from_text_to_kb_url(text, article_url, span_length=128, article_title=None,
+                    article_publish_date=None, verbose=False):
+  # tokenize whole text
+    inputs = tokenizer([text], return_tensors="pt")
+
+    # compute span boundaries
+    num_tokens = len(inputs["input_ids"][0])
+    if verbose:
+        print(f"Input has {num_tokens} tokens")
+    num_spans = math.ceil(num_tokens / span_length)
+    if verbose:
+        print(f"Input has {num_spans} spans")
+    overlap = math.ceil((num_spans * span_length - num_tokens) /
+                        max(num_spans - 1, 1))
+    spans_boundaries = []
+    start = 0
+    for i in range(num_spans):
+        spans_boundaries.append([start + span_length * i,
+                                 start + span_length * (i + 1)])
+        start -= overlap
+    if verbose:
+        print(f"Span boundaries are {spans_boundaries}")
+
+    # transform input with spans
+    tensor_ids = [inputs["input_ids"][0][boundary[0]:boundary[1]]
+                  for boundary in spans_boundaries]
+    tensor_masks = [inputs["attention_mask"][0][boundary[0]:boundary[1]]
+                    for boundary in spans_boundaries]
+    inputs = {
+        "input_ids": torch.stack(tensor_ids),
+        "attention_mask": torch.stack(tensor_masks)
+    }
+
+    # generate relations
+    num_return_sequences = 3
+    gen_kwargs = {
+        "max_length": 256,
+        "length_penalty": 0,
+        "num_beams": 3,
+        "num_return_sequences": num_return_sequences
+    }
+    generated_tokens = model.generate(
+        **inputs,
+        **gen_kwargs,
+    )
+
+    # decode relations
+    decoded_preds = tokenizer.batch_decode(generated_tokens,
+                                           skip_special_tokens=False)
+    # create kb
+    kb = KB_url()
+    i = 0
+    for sentence_pred in decoded_preds:
+        current_span_index = i // num_return_sequences
+        relations = extract_relations_from_model_output(sentence_pred)
+        for relation in relations:
+            relation["meta"] = {
+                article_url: {
+                    "spans": [spans_boundaries[current_span_index]]
+                }
+            }
+            kb.add_relation(relation, article_title, article_publish_date)
+        i += 1
+
+    return kb
+
+def get_article(url):
+  article = Article(url)
+  article.download()
+  article.parse()
+  return article
+
+def from_url_to_kb(url):
+  article = get_article(url)
+  config = {
+      "article_title" : article.title,
+      "article_publish_date" : article.publish_date
+  }
+  kb = from_text_to_kb_url(article.text, article.url, **config)
+  return kb
+
+# build a KB from multiple news links
+def from_urls_to_kb(urls, verbose=False):
+    kb = KB_url()
+    if verbose:
+        print(f"{len(urls)} links to visit")
+    for url in urls:
+        if verbose:
+            print(f"Visiting {url}...")
+        try:
+            kb_url = from_url_to_kb(url)
+            kb.merge_with_kb(kb_url)
+        except ArticleException:
+            if verbose:
+                print(f"  Couldn't download article at url {url}")
+    return kb
+
 # from KB to HTML visualization
 def save_network_html(kb, filename="network.html"):
     # create network
@@ -163,12 +262,19 @@ def save_network_html(kb, filename="network.html"):
     net.show(filename)
 
 if __name__ == '__main__':
-    text = """
-    Napoleon Bonaparte (born Napoleone di Buonaparte; 15 August 1769 – 5 May 1821), and later known by his regnal name Napoleon I, was a French military and political leader who rose to prominence during the French Revolution and led several successful campaigns during the Revolutionary Wars. He was the de facto leader of the French Republic as First Consul from 1799 to 1804. As Napoleon I, he was Emperor of the French from 1804 until 1814 and again in 1815. Napoleon's political and cultural legacy has endured, and he has been one of the most celebrated and controversial leaders in world history. Napoleon was born on the island of Corsica not long after its annexation by the Kingdom of France.[5] He supported the French Revolution in 1789 while serving in the French army, and tried to spread its ideals to his native Corsica. He rose rapidly in the Army after he saved the governing French Directory by firing on royalist insurgents. In 1796, he began a military campaign against the Austrians and their Italian allies, scoring decisive victories and becoming a national hero. Two years later, he led a military expedition to Egypt that served as a springboard to political power. He engineered a coup in November 1799 and became First Consul of the Republic. Differences with the British meant that the French faced the War of the Third Coalition by 1805. Napoleon shattered this coalition with victories in the Ulm Campaign, and at the Battle of Austerlitz, which led to the dissolving of the Holy Roman Empire. In 1806, the Fourth Coalition took up arms against him because Prussia became worried about growing French influence on the continent. Napoleon knocked out Prussia at the battles of Jena and Auerstedt, marched the Grande Armée into Eastern Europe, annihilating the Russians in June 1807 at Friedland, and forcing the defeated nations of the Fourth Coalition to accept the Treaties of Tilsit. Two years later, the Austrians challenged the French again during the War of the Fifth Coalition, but Napoleon solidified his grip over Europe after triumphing at the Battle of Wagram. Hoping to extend the Continental System, his embargo against Britain, Napoleon invaded the Iberian Peninsula and declared his brother Joseph King of Spain in 1808. The Spanish and the Portuguese revolted in the Peninsular War, culminating in defeat for Napoleon's marshals. Napoleon launched an invasion of Russia in the summer of 1812. The resulting campaign witnessed the catastrophic retreat of Napoleon's Grande Armée. In 1813, Prussia and Austria joined Russian forces in a Sixth Coalition against France. A chaotic military campaign resulted in a large coalition army defeating Napoleon at the Battle of Leipzig in October 1813. The coalition invaded France and captured Paris, forcing Napoleon to abdicate in April 1814. He was exiled to the island of Elba, between Corsica and Italy. In France, the Bourbons were restored to power. However, Napoleon escaped Elba in February 1815 and took control of France.[6][7] The Allies responded by forming a Seventh Coalition, which defeated Napoleon at the Battle of Waterloo in June 1815. The British exiled him to the remote island of Saint Helena in the Atlantic, where he died in 1821 at the age of 51. Napoleon had an extensive impact on the modern world, bringing liberal reforms to the many countries he conquered, especially the Low Countries, Switzerland, and parts of modern Italy and Germany. He implemented liberal policies in France and Western Europe.
-    """
 
-    kb = from_text_to_kb(text, verbose=True)
-    kb.print()
+    choice = input("How do you want to make KG? From URL or Text: ")
+    if(choice == "text"):
+        text = "Hi my name is Anshi. I study at IIIT,Hyderabad."
+
+        kb = from_text_to_kb(text, verbose=True)
+        kb.print()
+
+    if(choice == "url"):
+        # news_links = ["https://journals.lww.com/md-journal/fulltext/2025/04040/mendelian_based_urolithiasis_risk_concerning_fish.1.aspx?context=latestarticles"]
+        news_links = ["https://finance.yahoo.com/news/microstrategy-bitcoin-millions-142143795.html","https://finance.yahoo.com/news/brazils-energy-minister-asks-petrobras-201522428.html"]
+        kb = from_urls_to_kb(news_links, verbose=True)
+        kb.print()
 
     filename = "network_3_google.html"
     save_network_html(kb, filename=filename)
